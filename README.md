774591af0e55319d862920dc5b1b67f3bf585a3e
Vipin Sharma <vipin2112work@gmail.com>
2024-06-30 21:57:21 +0530

    complete solution

---
+++ README.md
@@ -0,0 +1,28 @@
+
+## My submission for the Library App  project in the Intermediate HTML and CSS on The Odin Project. 
+
+
+### Solution Links
+
+- Live Site URL: [https://vipinsharmaa.github.io/Odin-Library/](https://vipinsharmaa.github.io/Odin-Library/)
+- Solution URL: [https://github.com/vipinsharmaa/Odin-Library](https://github.com/vipinsharmaa/Odin-Library)
+
+
+### Screenshot
+
+![](images/Screenshot.png)
+
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS Grid
+- Flexbox
+- Javascript
+
+
+## Authors
+
+- Twitter - [@codecrash007](https://www.twitter.com/codecrash007)
+- GitHub  - [@vipinsharmaa](https://github.com/vipinsharmaa)
+
--- script.js
+++ script.js
@@ -1,65 +1,75 @@
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
 
 const myLibrary = [];
+
 const addBook = document.querySelector('button#addBook');
 const dialog = document.querySelector("dialog");
-const tiles = document.querySelector(".tiles");
-const inputs = document.querySelectorAll('input');
-
 const form = document.querySelector("#bookForm");
+const tiles = document.querySelector(".tiles");
 
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const titleInput = form.elements.title;
-    const authorInput = form.elements.author;
-    const numOfPagesInput = form.elements.numOfPages;
-    const readYetInput = form.elements.readYet;
-    addBookToLibrary(titleInput.value, authorInput.value, numOfPagesInput.value, readYetInput.value);
-    titleInput.value = '';
-    authorInput.value = '';
-    numOfPagesInput.value = '';
-    readYetInput.value = '';
-    dialog.close();
-    console.log(myLibrary);
-
-});
-
-
+// show addbook modal
 addBook.addEventListener("click", () => {
     dialog.showModal();
 });
 
+// Add book to Library
+function addBookToLibrary() {
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#numOfPages").value;
+    let read = document.querySelector("#read").checked;
 
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
 
-// constructor 
-function Book(title, author, numOfPages, readYet) {
-    this.title = title,
-        this.author = author,
-        this.numOfPages = numOfPages,
-        this.readYet = readYet,
-        this.info = function () {
-            return `${this.title} by ${this.author}, ${this.numOfPages} pages, ${readYet}`;
-        }
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    const book = new Book(title, author, pages, read);
-    myLibrary.push(book);
-    displayBook(title, author, pages, read);
-}
+// Form Submission handler
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    addBookToLibrary();
+    form.reset();
+    dialog.close();
+    displayBook();
+})
 
-function displayBook(title, author, pages, read) {
-    const newBook = document.createElement('div');
-    const titleTag = document.createElement('p');
-    const authorTag = document.createElement('p');
-    const pagesTag = document.createElement('p');
-    const readTag = document.createElement('p');
-    titleTag.append(`Title: ${title}`);
-    authorTag.append(author);
-    pagesTag.append(pages);
-    readTag.append(read);
-    newBook.append(titleTag);
-    newBook.append(authorTag);
-    newBook.append(pagesTag);
-    newBook.append(readTag);
-    tiles.append(newBook);
+
+// Display Books
+function displayBook() {
+    tiles.innerHTML = '';
+    for (let i = 0; i < myLibrary.length; i++) {
+        const book = document.createElement("div");
+        book.classList.add('book');
+        book.innerHTML = `
+        <div class="book" data-index=${i}>
+            <div class="book-info">
+                <h3>Title:</h3><span class="title">${myLibrary[i].title}</span>
+                <h3>Author:</h3> <span class="author">${myLibrary[i].author}</span>
+                <h3>Pages:</h3> <span class="pages">${myLibrary[i].pages}</span>
+                <h3>Status:</h3> <button class="read">${myLibrary[i].read ? "Read" : "Unread"}</button>
+            </div>
+            <button type="button" class="deleteBook">Delete</button>
+        </div>
+        `;
+
+        // toggle read status
+        book.querySelector(".read").addEventListener("click", () => {
+            myLibrary[i].read = !myLibrary[i].read;
+            displayBook();
+        });
+
+        // Delete Book
+        book.querySelector(".deleteBook").addEventListener("click", () => {
+            myLibrary.splice(i, 1);
+            displayBook();
+        });
+
+        tiles.appendChild(book);
+    }
+
 }
